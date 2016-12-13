6599f06782a8ae393f65e4ceaabb1a1a3b861f7b
Christian De Castro <robert12102000@yahoo.com>
2016-12-13 11:27:38 -0800

    added fixes
    added fixes

--- project7.js
+++ project7.js
@@ -12,11 +12,23 @@ function transformPoint(event) {
 
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(x, y, size, color) {
-  // square drawing code here
+  var newrect = document.createElementNS(namespace,"rect")
+  newrect.setAttribute("x",x);
+  newrect.setAttribute("y",y);
+  newrect.setAttribute("width",size)
+  newrect.setAttribute("height",size)
+  newrect.setAttribute("fill",color);
+  canvas.appendChild(newrect);
 }
 
 function drawCircle(x,y,size, color) {
-  
+  var newCircle = document.createElementNS(namespace,"circle")
+  newCircle.setAttribute("x",x);
+  newCircle.setAttribute("y",y);
+  newCircle.setAttribute("width",size)
+  newCircle.setAttribute("height",size)
+  newCircle.setAttribute("fill",color);
+  canvas.appendChild(newCircle);
 }
 
 // Step 3: Event listeners
